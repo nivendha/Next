bf40a2a9c6eaf7964148a8ff5ec0471401818154
nivendha <knivendha@gmail.com>
2015-08-25 00:24:20 +0530

    yet to start db

---
+++ src/js/database.js
@@ -0,0 +1,7 @@
+/*var bcrypt = require('bcryptjs');
+var crypto = require('crypto');
+var uuidhash = require('node-uuid');
+var pg = require('pg');
+
+var databaseUrl = (process.env.PG_url || 'postgres://postgres:root@localhost/green);
+*/
